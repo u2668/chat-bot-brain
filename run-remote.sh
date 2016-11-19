5519497f826afa73af75af8b7acb91bca20324bb
Alexandr Shushunov <ashushunov@mail.ru>
2016-11-19 12:05:51 +0300

    fix remote deploy script

--- run-remote.sh
+++ run-remote.sh
@@ -2,4 +2,4 @@
 deployAddress="46.101.204.43"
 ssh root@${deployAddress} docker rm -f brain
 ssh root@${deployAddress} docker pull u2668/chat-bot-brain
-ssh root@${deployAddress} run --name web-presentation -p 8081:80 --net go-to-canteen -d u2668/chat-bot-brain
+ssh root@${deployAddress} docker run --name web-presentation -p 8081:80 --net go-to-canteen -d u2668/chat-bot-brain
