7dace3a93cd9bb381d6ef3fe345389a206aed0c7
Alexandr Shushunov <ashushunov@mail.ru>
2016-11-24 19:50:14 +0300

    fix deploy scripts

--- run-local.sh
+++ run-local.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker rm -f brain
 docker pull u2668/chat-bot-brain
-docker run --name brain -p --net go-to-canteen 8080:8080 -d u2668/chat-bot-brain
+docker run --name brain -p 8080:8080 -d u2668/chat-bot-brain
 
 # for debug
 #docker exec -it brain /bin/bash
--- run-remote.sh
+++ run-remote.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-deployAddress="46.101.204.43"
+deployAddress="188.166.61.20"
 ssh root@${deployAddress} docker rm -f brain
 ssh root@${deployAddress} docker pull u2668/chat-bot-brain
-ssh root@${deployAddress} docker run --name brain -p 8081:8080 --net go-to-canteen -d u2668/chat-bot-brain
+ssh root@${deployAddress} docker run --name brain -p 8080:8080 -d u2668/chat-bot-brain
